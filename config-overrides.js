cee99d82e53ea2c7566250f3d05b10c94f8708cd
oddcommit <abdusametovartur@gmail.com>
2024-12-03 14:50:52 +0100

    fixed errors of metamask wallet

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,25 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = function override(config) {
-
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    buffer: require.resolve('buffer'),
+    crypto: require.resolve("crypto-browserify"),
+    buffer: require.resolve("buffer"),
+    process: require.resolve("process"),
+    stream: require.resolve("stream-browserify"),
+    vm: false,
   };
 
+  // Add plugins
   config.plugins = config.plugins || [];
 
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process",
     }),
   ];
 
